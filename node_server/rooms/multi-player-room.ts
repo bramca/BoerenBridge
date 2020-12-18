e3cf4c853d57836c6b10b7cb870dc92e42ca6118
Bram Cautaerts <bramcautaerts@gmail.com>
2020-12-18 22:55:13 +0100

    placing every client on table, and dealing cards

--- node_server/rooms/multi-player-room.ts
+++ node_server/rooms/multi-player-room.ts
@@ -43,7 +43,7 @@ export class Player extends Schema {
 export class State extends Schema {
     @type({ map: Player })
     players = new MapSchema<Player>();
-    clients = new MapSchema<Client>();
+    cls = new MapSchema<Client>();
     players_arr = [];
     cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "1"];
     suits = ["h", "d", "s", "c"];
@@ -52,6 +52,7 @@ export class State extends Schema {
     curr_round = 0;
     n_cards = 7;
     dealer = 0;
+    deck_index = 0;
     // something = "This attribute won't be sent to the client-side";
     constructor() {
         super();
@@ -65,7 +66,7 @@ export class State extends Schema {
     start_round() {
         this.shuffle_deck();
         console.log(this.deck);
-        let index = 0;
+        let index = deck_index;
         for (let j = 0; j < this.n_cards; j++) {
             for (let i = 0; i < this.players_arr.length; i++) {
                 let player = this.players_arr[(this.dealer + i  + 1) % this.players_arr.length];
@@ -73,6 +74,11 @@ export class State extends Schema {
                 index = player.draw(this.deck, index, 1);
             }
         }
+        deck_index = index;
+    }
+
+    get_trump_card() {
+        return deck[deck_index];
     }
 
     createPlayer(is_ai: boolean, sessionId: string) {
@@ -110,12 +116,18 @@ export class MultiPlayerRoom extends Room<State> {
     maxClients = 4;
     onCreate (options) {
         console.log("MultiPlayerRoom created!", options);
+        console.log("max spelers: " + this.maxClients);
 
         this.setState(new State());
 
         this.onMessage("name", (client, data) => {
             console.log("MultiPlayerRoom received message from", client.sessionId, ":", data);
             this.state.players.get(client.sessionId).name = data.name;
+            for (let player of this.state.players) {
+                if (player[1].id != client.sessionId) {
+                    this.state.cls[player[1].id].send("broadcast_name", {"id": client.sessionId, "name": data.name})
+                }
+            }
         });
         this.onMessage("start_game", (client, data) => {
             console.log("MultiPlayerRoom received message from", client.sessionId, ":", data);
@@ -125,18 +137,22 @@ export class MultiPlayerRoom extends Room<State> {
                 this.state.createPlayer(true, "bot" + (this.state.players.size + 1).toString());
                 this.state.players["bot" + this.state.players.size].name += this.state.players.size + 1;
             }
-            console.log("max spelers")
-            console.log(this.maxClients);
+            // lock the room when the game starts
+            this.lock();
+            console.log("max spelers: " + this.maxClients);
             this.state.start_round();
             // round started
             console.log("round started");
+            this.broadcast("deck_shuffled", this.state.deck);
             for (let player of this.state.players) {
                 console.log(player[1].toString());
                 if (!player[1].is_ai) {
-                    console.log("sending draw cards message to " + player[1].toString() + "with id: " + player[1].id);
-                    this.state.clients[player[1].id].send("draw_cards", player[1].cards);
+                    console.log("sending draw cards message to " + player[1].toString() + " with id: " + player[1].id);
+                    this.state.cls[player[1].id].send("draw_cards", player[1].cards);
                 }
             }
+            console.log("trump card: " + this.state.get_trump_card());
+            this.broadcast("trump_card", this.state.get_trump_card());
         });
     }
 
@@ -145,9 +161,17 @@ export class MultiPlayerRoom extends Room<State> {
     }
 
     onJoin (client: Client) {
-        client.send("ask_name", {});
-        this.state.clients[client.sessionId] = client;
+        console.log("client: " + client.sessionId + " joined.");
+        this.state.cls[client.sessionId] = client;
+        let player_names = {};
+        console.log("___ players already in the room ___");
+        for (let player of this.state.players) {
+            console.log("player: " + player[1].name + ", id: " + player[1].id);
+            player_names[player[1].id] = player[1].name;
+        }
+        console.log("______");
         this.state.createPlayer(false, client.sessionId);
+        client.send("ask_name", {"id": client.sessionId, "player_names": player_names});
     }
 
     onLeave (client) {
--- node_server/static_js/game.js
+++ node_server/static_js/game.js
@@ -3,6 +3,48 @@ var host = window.document.location.host.replace(/:.*/, '');
 var client = new Colyseus.Client(location.protocol.replace("http", "ws") + "//" + host + (location.port ? ':' + location.port : ''));
 var room;
 var players = {};
+var name = "";
+var hand = {};
+var player_dirs = ["player_S", "player_W", "player_N", "player_E"];
+var player_dirs_index = 0;
+var client_player_id = 0;
+var table_name = "#card-table";
+var number_of_cards = 0;
+function render_cards(player_id) {
+    let place = players[player_id].place;
+    console.log("drawing cards for: " + player_id + ", place: " + place);
+    if (place == player_dirs[1]) {
+        players[player_id].hand = new cards.Hand({faceUp: false, y: 280, x: -10});
+        for (let i = 0; i < number_of_cards; i++) {
+            let card_to_add = new cards.Card('c', i, table_name);
+            players[player_id].hand.addCard(card_to_add);
+        }
+        players[player_id].hand.render();
+    } else if (place == player_dirs[2]) {
+        players[player_id].hand = new cards.Hand({faceUp: false, y: 50, x: 270});
+        for (let i = 0; i < number_of_cards; i++) {
+            let card_to_add = new cards.Card('c', i, table_name);
+            players[player_id].hand.addCard(card_to_add);
+        }
+        players[player_id].hand.render();
+    } else if (place == player_dirs[3]) {
+        players[player_id].hand = new cards.Hand({faceUp: false, y: 280, x: 550});
+        for (let i = 0; i < number_of_cards; i++) {
+            let card_to_add = new cards.Card('c', i, table_name);
+            players[player_id].hand.addCard(card_to_add);
+        }
+        players[player_id].hand.render();
+    }
+}
+function getPlayersOnline() {
+    let count = 0;
+    for (let player_id of Object.keys(players)) {
+        if (!player_id.startsWith("bot")) {
+            count++;
+        }
+    }
+    return count;
+}
 client.joinOrCreate("multi_player").then(room_instance => {
     room = room_instance;
     game_started = false;
@@ -11,15 +53,14 @@ client.joinOrCreate("multi_player").then(room_instance => {
     // $("#decide-tricks-form").hide();
     console.log(Object.keys(players).length);
     // $("#decide-tricks-form").show();
-    $("#card-table").show();
-    $("#player_S").text(name);
-    $(".players").text(Object.keys(players).length);
-    deck.addCards([new cards.Card('s', 12, "#card-table")]);
+    $(table_name).show();
+    $(".players").text(getPlayersOnline());
+    // deck.addCards([new cards.Card('s', 12, table_name)]);
     deck.y = 200;
     deck.x = 250;
     discardPile.x = deck.x + 80;
     discardPile.y = deck.y;
-    discardPile.addCards([new cards.Card('s', 12, "#card-table")]);
+    // discardPile.addCards([new cards.Card('s', 12, table_name)]);
     deck.render({immediate:true});
     discardPile.render();
     // deck[0].rotate(90);
@@ -32,25 +73,93 @@ client.joinOrCreate("multi_player").then(room_instance => {
         }
     });
     room.state.players.onAdd = function (player, sessionId) {
-        players[sessionId] = player;
+        players[sessionId] = {};
+        console.log("player added: " + sessionId + ", place: " + players[sessionId].place + ", name: " + player.name);
         if (!sessionId.startsWith("bot")) {
-            $(".players").text(Object.keys(players).length);
+            $(".players").text(getPlayersOnline());
+        } else {
+            players[sessionId].place = player_dirs[player_dirs_index];
+            players[sessionId].name = sessionId;
+            player_dirs_index++;
+            $("#" + players[sessionId].place).text(sessionId);
+            let player_id = sessionId;
+            render_cards(player_id);
         }
     }
 
     room.state.players.onRemove = function (player, sessionId) {
-
+        console.log("player removed: " + sessionId);
+        $("#" + players[sessionId].place).text("");
+        delete players[sessionId];
+        player_dirs_index--;
+        $(".players").text(getPlayersOnline());
     }
 
+    room.onMessage("broadcast_name", (message) => {
+        let data_id = message.id;
+        let data_name = message.name;
+        players[data_id].place = player_dirs[player_dirs_index];
+        player_dirs_index++;
+        players[data_id].name = data_name;
+        $("#" + players[data_id].place).text(data_name);
+    });
 
     room.onMessage("ask_name", (message) => {
-        var name = prompt("What is your name?");
+        let client_id = message.id;
+        client_player_id = message.id;
+        let player_names = message.player_names;
+        let curr_players_length = Object.keys(player_names).length;
+        players[client_id].place = player_dirs[player_dirs_index];
+        player_dirs_index++;
+        let dirs_index_temp = 0;
+        for (let player_key of Object.keys(player_names)) {
+            players[player_key].name = player_names[player_key];
+            players[player_key].place = player_dirs[player_dirs.length - (curr_players_length - dirs_index_temp)];
+            dirs_index_temp++;
+            $("#" + players[player_key].place).text(players[player_key].name);
+        }
+        name = prompt("What is your name?");
+        players[client_id].name = name;
+        $("#" + players[client_id].place).text(name);
         room.send("name", { name: name });
+    });
+    room.onMessage("deck_shuffled", (message) => {
+        console.log("message");
+        console.log(message);
+        $(".buttons").hide();
+        game_started = true;
+        var deck_cards = [];
+        for (let card of message) {
+            let card_number = card.substring(0, card.length - 1);
+            let card_type = card[card.length - 1];
+            deck_cards.push(new cards.Card(card_type, card_number, table_name));
+        }
+        deck.addCards(deck_cards);
+        deck.render({immediate: true});
+        console.log("deck");
+        console.log(deck);
     });
     room.onMessage("draw_cards", (message) => {
         console.log(message);
+        players[client_player_id].hand = new cards.Hand({faceUp: true, y: 410, x: 270});
+        number_of_cards = message.length;
         for (let card of message) {
-            console.log(card);
+            let card_number = card.substring(0, card.length - 1);
+            let card_type = card[card.length - 1];
+            if (card_number == "K") {
+                card_number = 13;
+            } else if (card_number == "D") {
+                card_number = 12;
+            } else if (card_number == "J") {
+                card_number = 11;
+            }
+            players[client_player_id].hand.addCard(new cards.Card(card_type, card_number, table_name));
+            players[client_player_id].hand.render({});
+        }
+        for (let player_id of Object.keys(players)) {
+            if (players[player_id].place) {
+                render_cards(player_id);
+            }
         }
     });
 
